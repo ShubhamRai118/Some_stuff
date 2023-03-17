2cce25aca9f940e9630d7b11595204991f6b4964
RooGhz720 <rooghz720@gmail.com>
2023-03-18 04:08:06 +0800

    add timezone
    Signed-off-by: RooGhz720 <rooghz720@gmail.com>

--- nonrx/miui-noln8k.sh
+++ nonrx/miui-noln8k.sh
@@ -63,6 +63,7 @@ AnyKernelbranch="master"
 HOSST="MyLabs"
 USEER="RooGhz720"
 ID="25"
+TZY="Asia/Jakarta"
 MESIN="Git Workflows"
 
 # clang config
@@ -147,6 +148,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
 
+export TZ="$TZY"
 export KBUILD_BUILD_HOST="$HOSST"
 export KBUILD_BUILD_USER="$USEER"
 export KBUILD_BUILD_VERSION="$ID"
--- nonrx/oss.sh
+++ nonrx/oss.sh
@@ -63,6 +63,7 @@ AnyKernelbranch="master"
 HOSST="MyLabs"
 USEER="aghisna"
 ID="25"
+TZY="Asia/Jakarta"
 MESIN="Git Workflows"
 
 # clang config
@@ -146,6 +147,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export HEADER_ARCH=arm64
 
+export TZ="$TZY"
 export KBUILD_BUILD_HOST="$HOSST"
 export KBUILD_BUILD_USER="$USEER"
 export KBUILD_BUILD_VERSION="$ID"
