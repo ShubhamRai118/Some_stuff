3f0258440bf42fae462ef6bcb974680256834dc2
ShubhamRai118 <134160025+ShubhamRai118@users.noreply.github.com>
2023-07-22 23:46:14 +0530

    Update oss.sh

--- nonrx/oss.sh
+++ nonrx/oss.sh
@@ -44,7 +44,7 @@ export CHATID API_BOT TYPE_KERNEL
 
 # Kernel build config
 TYPE="OSS"
-KERNEL_NAME="Dark-TEST-OSS"
+KERNEL_NAME="Dark-NewGen"
 DEVICE="Redmi note 10 pro"
 DEFCONFIG="sweet_defconfig"
 AnyKernel="https://github.com/ShubhamRai118/Anykernel3"
