6be81ccdbc67f8b8b3a8764ed34923d8702458e4
ShubhamRai118 <134160025+ShubhamRai118@users.noreply.github.com>
2023-07-22 23:44:29 +0530

    Update oss.sh

--- nonrx/oss.sh
+++ nonrx/oss.sh
@@ -43,7 +43,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 export CHATID API_BOT TYPE_KERNEL
 
 # Kernel build config
-TYPE="MIUI"
+TYPE="OSS"
 KERNEL_NAME="Dark-TEST-OSS"
 DEVICE="Redmi note 10 pro"
 DEFCONFIG="sweet_defconfig"
