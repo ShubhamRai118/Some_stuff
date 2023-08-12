253e0a8a69a0c0dbb1f575180fac6af16e002491
ShubhamRai118 <134160025+ShubhamRai118@users.noreply.github.com>
2023-08-12 10:51:45 +0530

    Update oss.sh

--- nonrx/oss.sh
+++ nonrx/oss.sh
@@ -44,7 +44,7 @@ export CHATID API_BOT TYPE_KERNEL
 
 # Kernel build config
 TYPE="OSS"
-KERNEL_NAME="OXYGEN++_Test-GPU-HighPerf"
+KERNEL_NAME="Dark_Kernel-High-BrightnessTest"
 DEVICE="Redmi note 10 pro"
 DEFCONFIG="sweet_defconfig"
 AnyKernel="https://github.com/ShubhamRai118/Anykernel3"
