8224f51f50e04756e0e4217855b9e4c85166221b
ShubhamRai118 <134160025+ShubhamRai118@users.noreply.github.com>
2023-10-21 16:16:13 +0530

    Name change

--- nonrx/oss.sh
+++ nonrx/oss.sh
@@ -44,7 +44,7 @@ export CHATID API_BOT TYPE_KERNEL
 
 # Kernel build config
 TYPE="OSS"
-KERNEL_NAME="Dark_Kernel_MiUi"
+KERNEL_NAME="DO_Not_Flash_Im-NotRisponsible"
 DEVICE="Redmi note 10 pro"
 DEFCONFIG="sweet_defconfig"
 AnyKernel="https://github.com/ShubhamRai118/Anykernel3"
